29f95ca66e79be231dd82a3f708980ced20c862a
ohAlee <alebartoh@gmail.com>
2023-02-28 10:20:45 +0100

    Improvements

--- proxy/src/main/java/com/velocitypowered/proxy/command/builtin/VelocityCommand.java
+++ proxy/src/main/java/com/velocitypowered/proxy/command/builtin/VelocityCommand.java
@@ -54,7 +54,6 @@ import java.util.stream.Collectors;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TranslatableComponent;
-import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
